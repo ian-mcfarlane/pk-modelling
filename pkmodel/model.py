a0c45dcf4227a155c808d130ebfd1e3a1ccdf53f
Ollie Turnbull <oturnbull1@gmail.com>
2021-10-20 14:02:59 +0100

    First creation of TwoComponentModel

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -4,7 +4,7 @@ A model class for solving PK calculatins given a Protocol data class.
 from abc import ABC, abstractmethod
 from solution import Solution
 from protocol import Protocol
-
+import scipy.integrate
 
 class AbstractModel(ABC):
     """An abstract base (PK) model
@@ -17,7 +17,7 @@ class AbstractModel(ABC):
 
     """
     def __init__(self, protocol: Protocol):
-        self.__protocol = protocol
+        pass
 
     @abstractmethod
     def solve_ode(self):
@@ -27,3 +27,55 @@ class AbstractModel(ABC):
     @abstractmethod
     def get_solution(self) -> Solution:
         pass
+
+
+class TwoCompartmentModel(AbstractModel):
+
+    def __init__(self, protocol: Protocol):
+        self.protocol = protocol
+        self.solution = self.solve_ode()
+
+    def rhs(self, t, y):
+        """[summary]
+
+        :param t: [description]
+        :type t: [type]
+        :param y: [description]
+        :type y: [type]
+        :return: [description]
+        :rtype: [type]
+        """        
+        Q_p1 = self.protocol.Q_p1
+        V_c = self.protocol.V_c
+        V_p1 = self.protocol.V_p1
+        CL = self.protocol.CL
+        X = self.protocol.X
+
+        q_c, q_p1 = y
+        transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
+        dqc_dt = self.protocol.dose(t, X) - q_c / V_c * CL - transition
+        dqp1_dt = transition
+        return [dqc_dt, dqp1_dt]
+
+    def solve_ode(self):
+        """[summary]
+
+        :return: [description]
+        :rtype: [type]
+        """        
+        
+        sol = scipy.integrate.solve_ivp(
+            fun=lambda t, y: self.rhs(t, y),
+            t_span=[self.protocol.t_eval[0], self.protocol.t_eval[-1]],
+            y0=self.protocol.y0, t_eval=self.protocol.t_eval
+        ) 
+        return Solution(sol)
+    
+    @property
+    def get_solution(self) -> Solution:
+        """ Returns the Solution class of the solved PK model.
+
+        :return: Solution class used to display result.
+        :rtype: Solution
+        """        
+        return self.solution
