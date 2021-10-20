3cf13adee452d67930a6ef46bdeeb9f1e716a4d5
Elliot <e.barbeary@outlook.com>
2021-10-20 16:15:28 +0100

    Disable automated testing until testing suite is created Fixes #23

--- pkmodel/tests/test_protocol.py
+++ pkmodel/tests/test_protocol.py
@@ -10,6 +10,13 @@ class ProtocolTest(unittest.TestCase):
         """
         Tests Protocol creation.
         """
-        model = pk.Protocol()
-        self.assertEqual(model.value, 43)
+        model = pk.Protocol(1.1, 2.2, 3.3, 4.4, 5.5, 6, 7, 8)
+        self.assertEqual(model.Q_p1, 1.1)
+        self.assertEqual(model.V_c, 2.2)
+        self.assertEqual(model.V_p1, 3.3)
+        self.assertEqual(model.CL, 4.4)
+        self.assertEqual(model.X, 5.5)
+        self.assertEqual(model.dose_on, 6)
+        self.assertEqual(model.dose_off, 7)
+        self.assertEqual(model.k_a, 8.8)
 
