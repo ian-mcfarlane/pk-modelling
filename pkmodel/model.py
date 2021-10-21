e9f6044b62c6e7eabf38b52ac5c483bbfd10ac00
Hazel Wee <hazelweeling@gmail.com>
2021-10-21 09:43:45 +0100

    #1 used suggestions from linter: flake8.

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -6,6 +6,7 @@ from solution import Solution
 from protocol import Protocol
 import scipy.integrate
 
+
 class AbstractModel(ABC):
     """An abstract base (PK) model
 
@@ -80,6 +81,7 @@ class TwoCompartmentModel(AbstractModel):
         """        
         return self.solution
 
+
 class ThreeCompartmentModel(AbstractModel):
 
     def __init__(self, protocol: Protocol):
@@ -95,7 +97,7 @@ class ThreeCompartmentModel(AbstractModel):
         :type y: [type]
         :return: [description]
         :rtype: [type]
-        """        
+        """       
         Q_p1 = self.protocol.Q_p1
         V_c = self.protocol.V_c
         V_p1 = self.protocol.V_p1
@@ -105,8 +107,8 @@ class ThreeCompartmentModel(AbstractModel):
 
         q_o, q_c, q_p1 = y
         transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
-        dqo_dt = self.protocol.dose(t,X) - (k_a*q_o)
-        dqc_dt = k_a*q_o - (q_c*CL)/V_c - transition
+        dqo_dt = self.protocol.dose(t, X) - (k_a * q_o)
+        dqc_dt = k_a * q_o - (q_c * CL) / V_c - transition
         dqp1_dt = transition
         return [dqo_dt, dqc_dt, dqp1_dt]
 
