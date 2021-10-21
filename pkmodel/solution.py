d3de04ada3c4141994913af5078e1e02d1d40cf0
Mona Furukawa <mona.furukawa@new.ox.ac.uk>
2021-10-21 10:03:21 +0100

    Fixed some Linting errors

--- pkmodel/solution.py
+++ pkmodel/solution.py
@@ -1,29 +1,28 @@
 import matplotlib.pylab as plt
 import numpy as np
-import protocol as Protocol #
+from protocol import Protocol #CHECK WITH OLLIE
 
-Class Solution: 
+class Solution:
 
-
-    def __init__(self, sol,protocol:Protocol):
+    def __init__(self, sol,protocol:Protocol): #CHECK WITH OLLIE
         self.sol = sol
         graph()
         
-
     def graph (self):
         
-        if self.type_of_compartment== 2:
+        if self.no_of_compartment== 2:
             plt.plot(sol.t, sol.y[0, :], label=model['name'] + '- q_c')
-            plt.plot(sol.t, .solution.y[1, :], label=model['name'] + '- q_p1')
+            plt.plot(sol.t, sol.y[1, :], label=model['name'] + '- q_p1')
         
-        else type_of_compartment.self == 3:
-            plt.plot(sol.t, sol.y[0, :], label=model['name'] + '- q_o)
+        else:
+            plt.plot(sol.t, sol.y[0, :], label=model['name'] + '- q_o')
             plt.plot(sol.t, sol.y[1, :], label=model['name'] + '- q_c')
             plt.plot(sol.t, sol.y[2, :], label=model['name'] + '- q_p1')
 
         
 
         plt.legend()
+        plt.title("The change in drug quantity")
         plt.ylabel('drug mass [ng]')
         plt.xlabel('time [h]')
         plt.show()
