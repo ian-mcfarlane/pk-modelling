1db92891564d0073f1143d8568c8df4a2e58775e
Mona Furukawa <mona.furukawa@new.ox.ac.uk>
2021-10-21 10:15:31 +0100

    Fixed the Flake8 problems

--- pkmodel/solution.py
+++ pkmodel/solution.py
@@ -1,16 +1,16 @@
 import matplotlib.pylab as plt
-import numpy as np
-from protocol import Protocol #CHECK WITH OLLIE
+from protocol import Protocol
+
 
 class Solution:
-
-    def __init__(self, sol,protocol:Protocol): #CHECK WITH OLLIE
+    def __init__(self, sol, protocol: Protocol):
         self.sol = sol
-        graph()
+        self.graph()
+        self.protocol = Protocol
         
-    def graph (self):
+    def graph(self):
         
-        if self.no_of_compartment== 2:
+        if self.protocol.no_of_compartment == 2:
             plt.plot(sol.t, sol.y[0, :], label=model['name'] + '- q_c')
             plt.plot(sol.t, sol.y[1, :], label=model['name'] + '- q_p1')
         
@@ -18,8 +18,6 @@ class Solution:
             plt.plot(sol.t, sol.y[0, :], label=model['name'] + '- q_o')
             plt.plot(sol.t, sol.y[1, :], label=model['name'] + '- q_c')
             plt.plot(sol.t, sol.y[2, :], label=model['name'] + '- q_p1')
-
-        
 
         plt.legend()
         plt.title("The change in drug quantity")
