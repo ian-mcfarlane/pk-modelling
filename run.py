4a5c441fd5cb3d57fac18a3c152b5286a1b31ddb
Ollie Turnbull <oturnbull1@gmail.com>
2021-10-21 15:53:02 +0100

    Fixed float as string

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ def parse_args(argv=None):
     parser.add_argument('-d', '--data_root', type=str, required=False, help="Path to location of csv file (default = './'",
                         default='./')
     parser.add_argument('-f', '--file_name', type=str, required=False, help="Filename for csv file containing model parameters",
-                        default='test.csv')
+                        default='example.csv')
     args = parser.parse_args(argv)
     
     return args
@@ -27,20 +27,22 @@ if __name__ == "__main__":
     # Load csv files
     with open(Path(args.data_root + args.file_name)) as f:
         reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-        data = list(reader)
+        try:
+            data = list(reader)
+        except ValueError:
+            raise ValueError("Strings in CSVs should be in double quotes.")
     
     # Create a Model object for each line in the csv file
     models = []
     for model_params in data:
 
         # create protocol
-        print(model_params)
         protocol = pk.protocol.Protocol(*model_params)
 
         # Check if 2 or 3 component model
-        if model_params[0] == 2:
+        if model_params[1] == 2:
             models.append(pk.model.TwoCompartmentModel(protocol))
-        elif model_params[0] == 3:
+        elif model_params[1] == 3:
             models.append(pk.model.ThreeCompartmentModel(protocol))
 
         else:
