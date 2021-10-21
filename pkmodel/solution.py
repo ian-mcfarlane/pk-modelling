cccd677370f5e7799d85d8d180d253eede36fa97
Mona Furukawa <mona.furukawa@new.ox.ac.uk>
2021-10-21 12:21:21 +0100

    Changed the argument of init to models

--- pkmodel/solution.py
+++ pkmodel/solution.py
@@ -1,25 +1,28 @@
 import matplotlib.pylab as plt
-from .protocol import Protocol
+    
+    """This code will import the object model and graph the solutions."""
 
 
 class Solution:
-    def __init__(self, sol, protocol: Protocol):
-        self.sol = sol
-        self.protocol = protocol
+    def __init__(self, models):
+        self.models = models
         self.graph()
 
-        
+        """Looks at individual model created in model and prepares to plot the graph.
+        """
     def graph(self):
+        for model in models:
+            if model.protocol.comps == 2:
+                plt.plot(model.sol.t, model.sol.y[0, :], label="Two-compertment model" + '- q_c')
+                plt.plot(model.sol.t, model.sol.y[1, :], label="Two-compertment model" + '- q_p1')
         
-        if self.protocol.comps == 2:
-            plt.plot(self.sol.t, self.sol.y[0, :], label="Two-compertment model" + '- q_c')
-            plt.plot(self.sol.t, self.sol.y[1, :], label="Two-compertment model" + '- q_p1')
+            else:
+                plt.plot(model.sol.t, model.sol.y[0, :], label="Three-comparmtent model" + '- q_o')
+                plt.plot(model.sol.t, model.sol.y[1, :], label="Three-comparmtent model" + '- q_c')
+                plt.plot(model.sol.t, model.sol.y[2, :], label="Three-comparmtent model" + '- q_p1')
         
-        else:
-            plt.plot(self.sol.t, self.sol.y[0, :], label="Three-comparmtent model" + '- q_o')
-            plt.plot(self.sol.t, self.sol.y[1, :], label="Three-comparmtent model" + '- q_c')
-            plt.plot(self.sol.t, self.sol.y[2, :], label="Three-comparmtent model" + '- q_p1')
-
+        """The graph will be plotted using the code below
+        """
         plt.legend()
         plt.title("The change in drug quantity")
         plt.ylabel('drug mass [ng]')
