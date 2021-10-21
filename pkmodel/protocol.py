ff2ed547896ec678e08ab941451e5b500ed90b36
Elliot <e.barbeary@outlook.com>
2021-10-21 16:14:55 +0100

    Fix linting issues Fixes #75

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -9,33 +9,24 @@ import matplotlib.pyplot as plt
 class Protocol:
     """A Pharmokinetic (PK) protocol
 
-    Contains all parameters needed to run 2- or 3-compartment models, and dose
-        function.
+    Contains all parameters needed to run 2- or 3-compartment models, and dose function.
 
-    :param comps: int number indicating which model (2- or 3-compartment) to be
-        used
-    :param Q_p1: float transition rate between central component and
-        peripheral component
+    :param comps: int number indicating which model (2- or 3-compartment) to be used
+    :param Q_p1: float transition rate between central component and peripheral component
     :param V_c: float volume of central component
     :param V_p1: float volume of peripheral component
     :param CL: float elimination rate for drug in central component
     :param X: float amount of drug administered
     :param dose_on: int number of time units (thousandths of an hour)
-        for which drug is administered at a time (dose_on = 0 -> single dose
-        at time 0)
-        If dose_off = 0, dose_on can be 0 (instantaneous dose) or 1
-        (continuous dose)
-    :optional param dose_off: int number of time units (thousandths of an hour)
-        for which drug is not administered at a time
-        (default value 0 -> drug is continuously administered,
-        unless dose_on = 0 which overrides this)
-    :optional param k_a: float for three-component model only absorption rate
-        for subcutaneous dosing
-    :optional param graph_preview: bool to trigger graph preview of dosing
-        function (default value False -> preview not shown)
+        for which drug is administered at a time (dose_on = 0 -> single dose at time 0)
+        If dose_off = 0, dose_on can be 0 (instantaneous dose) or 1 (continuous dose)
+    :optional param dose_off: int number of time units (thousandths of an hour) for which drug is not administered at a time
+        (default value 0 -> drug is continuously administered, unless dose_on = 0 which overrides this)
+    :optional param k_a: float for three-component model only absorption rate for subcutaneous dosing
+    :optional param graph_preview: bool to trigger graph preview of dosing function (default value False -> preview not shown)
 
-    :method dose: takes parameters t, X and returns X or 0, depending on value
-        of t. Represents a variable dose function over time.
+    :method dose: takes parameters t, X and returns X or 0, depending on value of t.
+        Represents a variable dose function over time.
     """
 
     def __init__(self, label: str, comps: int, Q_p1: float, V_c: float,
