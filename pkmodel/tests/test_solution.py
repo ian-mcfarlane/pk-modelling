9de53e1cd4234f488ae912dff94526f4e78a378b
Elliot <e.barbeary@outlook.com>
2021-10-22 14:37:36 +0100

    Improve coverage of unit tests for solution.py Fixes #116

--- pkmodel/tests/test_solution.py
+++ pkmodel/tests/test_solution.py
@@ -15,3 +15,17 @@ class SolutionTest(unittest.TestCase):
         solution = pk.Solution(models, True)
         self.assertEqual(solution.models[0], models[0])
 
+    def test_graph(self):
+        """
+        Tests Solution graph method.
+        """
+        models = [
+            pk.TwoCompartmentModel(pk.Protocol("test", 2, 1.1, 2.2, 3.3, 4.4, 5.5, 0, 7, 8, False)),
+            pk.TwoCompartmentModel(pk.Protocol("test", 2, 1.1, 2.2, 3.3, 4.4, 5.5, 6, 0, 8, False)),
+            pk.TwoCompartmentModel(pk.Protocol("test", 2, 1.1, 2.2, 3.3, 4.4, 5.5, 6, 7, 8, False)),
+            pk.ThreeCompartmentModel(pk.Protocol("test", 3, 1.1, 2.2, 3.3, 4.4, 5.5, 0, 7, 8, False)),
+            pk.ThreeCompartmentModel(pk.Protocol("test", 3, 1.1, 2.2, 3.3, 4.4, 5.5, 6, 0, 8, False)),
+            pk.ThreeCompartmentModel(pk.Protocol("test", 3, 1.1, 2.2, 3.3, 4.4, 5.5, 6, 7, 8, False))
+        ]
+        solution = pk.Solution(models, True)
+        solution.graph()
